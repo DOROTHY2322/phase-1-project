66a9a9c1796a1c5690ed3b43eafc125fea964c87
DOROTHY2322 <kimelidorothy@gmail.com>
2022-12-15 12:21:55 +0300

    fetching api

---
+++ index.js
@@ -0,0 +1,43 @@
+// // hide the sign-up form by default
+
+//   document.getElementById("signup-form").style.display = "none";
+
+//   // show the sign-up form when the user clicks on the "Sign Up" button
+
+//   document.getElementById("signup-button").addEventListener("click", function() {
+//     document.getElementById("signup-form").style.display = "block";
+//   });
+
+//   // hide the sign-up form when the user clicks on the "Close" button
+//   document.getElementById("close-button").addEventListener("click", function() {
+//     document.getElementById("signup-form").style.display = "none";
+//   });
+
+//URLS
+//random cocktails
+const randomCocktail= 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+//name of the cocktails
+const cocktailName ='https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita'
+//ingredients by name 
+const ingredientName = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?i=vodka'
+// filter by alcoholic
+const cocktailcategory ='https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic'
+//filter non alcoholic 
+const Type='https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic'
+//search
+const search = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin'
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    //to get random cocktails
+    function getRandomCocktail(){
+        fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+        .then((response)=>response.json())
+        .then((data)=>{
+            console.log(data)
+        })
+    }
+    getRandomCocktail();
+        
+
+})
